b39f5d80954fc7ea3c1337d7abbfe2dec0edf8fa
Tom Gehrke <phyrog@gmail.com>
2013-05-05 03:44:04 +0200

    Removed devel comment

--- source/dlirium/auth/oauth2.d
+++ source/dlirium/auth/oauth2.d
@@ -9,18 +9,6 @@ import vibe.stream.operations;
 import dlirium.data;
 import std.conv;
 
-/*
--1. string client_secret = ...; //siehe Github.
- 0. string client_id = "6abe9a9254818f547195"
- 1. GET https://github.com/login/oauth/authorize?client_id=#{client_id}
- 2. Antwort von Github: string code = bd0c1b2217a31f83dbca;
- 3. POST https://github.com/login/oauth/access_token
-    client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}
- 4. Antwort von Github: string access_token = aed30a770e27b16efd2559d6b53c73d1039d2845; string token_type = "bearer";
- 5. GET https://api.github.com/user?access_token=#{access_token}
- 6. Antwort von Github: JSON String mit Benutzerdaten
- */
-
 string requestAuthToken(OAuth2Provider provider, string code)
 {
     auto res = requestHTTP(provider.authTokenRequestURL, (scope req) {
